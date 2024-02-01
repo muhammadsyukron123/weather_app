821b947da2fd597444744e5bb31269b85b35875f
muhammadsyukron123 <muhammadsyukronabdillah@gmail.com>
2024-02-01 08:21:23 +0700

    Removing boilerplate code, improving UIs

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,6 @@ class _WeatherScreenState extends State<WeatherScreen> {
   double? _latitude;
   double? _longitude;
   double? _temperatureC;
-  String? _temperatureCStr;
-  String? _time;
-  String? _address;
   String? _city;
   String _iconUrl = '';
 
@@ -45,34 +42,26 @@ class _WeatherScreenState extends State<WeatherScreen> {
     _getMapImage();
   }
 
-
-
-  Future<String> _getAddress(double latitude, double longitude) async {
-    final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=YourAPIKey');
-    final response = await http.get(url);
-    final resData = json.decode(response.body);
-    final address = resData['results'][0]['formatted_address'];
-    return address;
-  }
-
-  Future<String> _getCity(double latitude, double longitude) async {
-    final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=YourAPIKey');
-    final response = await http.get(url);
-    final resData = json.decode(response.body);
-    final address = resData['results'][0]['address_components'][4]['long_name'];
-    return address;
-  }
-
   Future<Image> _getMapImage() async {
-    String url = 'https://maps.googleapis.com/maps/api/staticmap?center=$_latitude,$_longitude=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$_latitude,$_longitude&key=YourApiKey';
+    String url = 'https://maps.googleapis.com/maps/api/staticmap?center=$_latitude,$_longitude=&zoom=16&size=600x300&maptype=roadmap&markers=color:purple%7Clabel:A%7C$_latitude,$_longitude&key=YourAPIKey';
     return Image.network(url);
   }
 
   Future<void> _updateWeather() async {
     LocationPermission permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      setState(() {
+        _weather = 'Location permissions are denied.';
+      });
+      return;
+    }
+    if(permission == LocationPermission.whileInUse || permission == LocationPermission.always){
+      setState(() {
+        _weather = 'Location permissions are granted.';
+      });
+    }
+    else{
       setState(() {
         _weather = 'Location permissions are denied.';
       });
@@ -85,21 +74,18 @@ class _WeatherScreenState extends State<WeatherScreen> {
     _longitude = position.longitude;
     print('${position.latitude}, ${position.longitude}');
     final response = await http.get(
-        Uri.parse('http://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=yourappid'));
+        Uri.parse('http://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&units=metric&appid=YourAPIKey'));
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
       // await _getMapImage();
       String iconUrl = 'http://openweathermap.org/img/w/${data["weather"][0]["icon"]}.png';
       String weather = data['weather'][0]['description'];
-      double temperatureK = data['main']['temp'];
-      String address = await _getAddress(_latitude!, _longitude!);
-      String city = await _getCity(_latitude!, _longitude!);
-      _temperatureC = temperatureK - 273.15;
-      _temperatureCStr = _temperatureC!.toStringAsFixed(1);
+      double temperatureC = data['main']['temp'];
+      String city = data['name'];
+      _temperatureC = temperatureC;
 
       setState(() {
         _weather = weather;
-        _address = address;
         _city = city;
         _iconUrl = iconUrl;
       });
@@ -118,56 +104,55 @@ Widget build(BuildContext context) {
     ),
     body: RefreshIndicator(
       onRefresh: _updateWeather,
-      child: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              leading: _iconUrl == '' ? CircularProgressIndicator() : Image.network(_iconUrl),
-              title: Text('$_weather', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              subtitle: Text('$_city'),
-              trailing: Text('$_temperatureCStr °C', style: TextStyle(fontSize: 20),),
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                leading: _iconUrl == '' ? CircularProgressIndicator() : Image.network(_iconUrl),
+                title: Text('$_weather', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                subtitle: Text('$_city'),
+                trailing: Text('$_temperatureC °C', style: TextStyle(fontSize: 20),),
+              ),
             ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              // border: Border.all(
-              //   width: 1,
-              //   color: Theme.of(context).colorScheme.onSecondary.withOpacity(1),
-              // ),
-              color: Colors.white70,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white70,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  FutureBuilder<Image>(
+                    future: _getMapImage(),
+                    builder: (BuildContext context, AsyncSnapshot<Image> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return snapshot.data!;
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    },
+                  ),
+                  SizedBox(height: 5,),
+                  Text('latitude: $_latitude'),
+                  SizedBox(height: 5,),
+                  Text('longitude: $_longitude'),
+                ],
+              ),
             ),
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                FutureBuilder<Image>(
-                  future: _getMapImage(),
-                  builder: (BuildContext context, AsyncSnapshot<Image> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      return snapshot.data!;
-                    } else {
-                      return CircularProgressIndicator();
-                    }
-                  },
-                ),
-                SizedBox(height: 5,),
-                Text('latitude: $_latitude'),
-                SizedBox(height: 5,),
-                Text('longitude: $_longitude'),
-              ],
-            ),
-          ),
 
-        ],
+          ],
+        ),
       ),
     ),
   );
